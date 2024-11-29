8b57c388631ec8891f6766dce88fc5894789e113
Quercaesii <somsit.thanapat@gmail.com>
2024-11-29 12:42:10 +0700

    fix: progress file corruption

--- lib/services/models/ProgressFileStore.dart
+++ lib/services/models/ProgressFileStore.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:synchronized/synchronized.dart';
 
 abstract class ProgressStore {
   Future<void> set(String fingerprint, double progress);
@@ -16,19 +17,25 @@ class ProgressFileStore implements ProgressStore {
 
   final File file;
   Map<String, double> _progressMap = {};
+  final _lock = Lock();
 
   Future<void> _loadFromFile() async {
-    if (await file.exists()) {
-      final contents = await file.readAsString();
-      final Map<String, dynamic> jsonMap = jsonDecode(contents);
-      _progressMap = jsonMap.map((key, value) => MapEntry(key, value.toDouble()));
-    }
+    await _lock.synchronized(() async {
+      if (await file.exists()) {
+        final contents = await file.readAsString();
+        final Map<String, dynamic> jsonMap = jsonDecode(contents);
+        _progressMap = jsonMap.map((key, value) => MapEntry(key, value.toDouble()));
+      }
+    });
   }
 
+
   Future<void> _saveToFile() async {
-    final jsonMap = _progressMap.map((key, value) => MapEntry(key, value));
-    final contents = jsonEncode(jsonMap);
-    await file.writeAsString(contents, mode: FileMode.write);
+    await _lock.synchronized(() async {
+      final jsonMap = _progressMap.map((key, value) => MapEntry(key, value));
+      final contents = jsonEncode(jsonMap);
+      await file.writeAsString(contents, mode: FileMode.writeOnly, flush: true);
+    });
   }
 
   @override
