a8fa776af8619437c318d4ffe7f80c7412a1ef9d
Quercaesii <somsit.thanapat@gmail.com>
2024-12-02 11:39:24 +0700

    change: custom_button to camel case

--- lib/pages/InformationPage.dart
+++ lib/pages/InformationPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
-import '../component/custom_button.dart';
+import '../component/customButton.dart';
 import '../pages/LoginPage.dart';
 import '../services/models/SecureStorageCache.dart';
 
